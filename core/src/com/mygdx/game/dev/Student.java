16374f12edbfe7a209558e5f0646c5a0bb4d92b5
voidvvv <->
2022-07-25 23:15:49 +0800

    dev01

---
+++ core/src/com/mygdx/game/dev/Student.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.dev;
+
+/**
+ * @Classname Student
+ * @Description
+ * @Date 2022/7/25 23:15
+ * @Created by zkj
+ */
+public class Student {
+    private String name;
+
+    public Student() {
+        name = "dev01";
+    }
+}
