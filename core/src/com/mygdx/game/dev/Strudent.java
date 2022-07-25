499fe871140dc1a70fbf27a71396a81c5b66844b
voidvvv <->
2022-07-25 23:16:42 +0800

    dev02

---
+++ core/src/com/mygdx/game/dev/Strudent.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.dev;
+
+/**
+ * @Classname Strudent
+ * @Description
+ * @Date 2022/7/25 23:16
+ * @Created by zkj
+ */
+public class Strudent {
+    private String name;
+
+    public Strudent() {
+        name = "dev02";
+    }
+}
